f6ebee8c7c8d58723b88b56bb2a96c682429599d
Maël Ehrhard <mael.ehrhard@outlook.fr>
2024-09-17 15:28:48 +0200

    🔊 Add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NodeSDK as RainbowSDK } from 'rainbow-node-sdk/lib/NodeSDK';
 import { ConfigModule, ConfigService, ConfigType } from '@nestjs/config';
 import { AppConfig } from './app.config';
 import { config as defaultRainbowConfig } from 'rainbow-node-sdk/lib/config/config';
+import { AppLogger } from './common/services/app-logger.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true, load: [AppConfig] })],
   controllers: [AppController],
   providers: [
     AppService,
+    AppLogger,
     {
       provide: RainbowSDK,
       useFactory: async (
         service: ConfigService<ConfigType<typeof AppConfig>>,
       ) => {
+        const logger = new Logger('RainbowSDK');
         const appConfig = service.get('rainbow', { infer: true });
         const rainbowConfig = {
           ...defaultRainbowConfig,
@@ -42,6 +45,7 @@ import { config as defaultRainbowConfig } from 'rainbow-node-sdk/lib/config/conf
         };
         const sdk = new RainbowSDK(rainbowConfig);
         await sdk.start();
+        logger.log(`Connected to ${appConfig.host}`);
         return sdk;
       },
       inject: [ConfigService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, Optional } from '@nestjs/common';
 import { NodeSDK as RainbowSDK } from 'rainbow-node-sdk/lib/NodeSDK';
 import { Bubble } from 'rainbow-node-sdk/lib/common/models/Bubble';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly rainbow: RainbowSDK) {}
+  constructor(
+    private readonly rainbow: RainbowSDK,
+    @Optional()
+    private readonly logger = new Logger(AppService.name),
+  ) {}
 
   getBubbles(): Bubble[] {
+    this.logger.verbose('Getting all bubbles');
     return this.rainbow.bubbles.getAllBubbles();
   }
 }
