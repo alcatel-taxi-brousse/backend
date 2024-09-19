49da615a35022ddf526e6277379407eeb3dcce3e
Maël Ehrhard <mael.ehrhard@outlook.fr>
2024-09-19 10:05:58 +0200

    🔧 Resolve hostname alias in config

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,6 +1,13 @@
+const hosts = {
+  sandbox: 'sandbox.openrainbow.com',
+  official: 'openrainbow.com',
+};
+
 export const AppConfig = () => ({
   rainbow: {
-    host: process.env.RAINBOW_HOST || 'sandbox',
+    host:
+      (hosts[process.env.RAINBOW_HOST] || process.env.RAINBOW_HOST) ??
+      'sandbox.openrainbow.com',
     login: process.env.RAINBOW_LOGIN || 'login',
     password: process.env.RAINBOW_PASSWORD || 'password',
     appID: process.env.RAINBOW_APP_ID || 'appId',
