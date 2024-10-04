3e36bfb95c08b721e2117d7be305eb964698cef5
Zinouo <znd.odn@gmail.com>
2024-10-04 14:05:14 +0200

    quick changes

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('taxibrousse', 'zinedine', '', {
+const sequelize = new Sequelize('taxibrousse', 'username', 'password', {
     host: 'localhost',
     dialect: 'postgres',
     port: 5432,
