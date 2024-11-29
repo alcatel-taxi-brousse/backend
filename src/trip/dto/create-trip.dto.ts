11b8c19e3a7e1df8fd3b28f93f35025373caa0e4
Belhia Fouad <fouad51100@gmail.com>
2024-11-29 15:31:49 +0100

    ✨ Enhance Trip module with controller and DTO
    - Implemented TripController with methods for retrieving and creating trips.
    - Integrated TripService for business logic handling.
    - Added CreateTripDto for structured trip creation data validation.
    - Introduced TripModule to encapsulate trip-related components.

---
+++ src/trip/dto/create-trip.dto.ts
@@ -0,0 +1,28 @@
+import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateTripDto {
+  @IsNotEmpty()
+  @IsString()
+  start_location: string;
+
+  @IsNotEmpty()
+  @Type(() => Date)
+  @IsDate()
+  date: Date;
+
+  @IsNotEmpty()
+  @IsString()
+  frequence: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  nb_seats_car: number;
+
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  creator_user_id: number;
+}
