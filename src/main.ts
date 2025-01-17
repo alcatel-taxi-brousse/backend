018665877cb4ebc18f346544d0fd4314b1dd8eba
OfferleA <alexandre.offerle@itwautomotive.com>
2025-01-17 15:30:43 +0100

    Utilisation du port bindé 3000 par nestJS pour ensuite bind dans render.com

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap(): Promise<void> {
     enableSwaggerEndpoint(app);
   }
 
-  await app.listen(443);
+  await app.listen(3000);
 }
 
 function enableSwaggerEndpoint(app: INestApplication): void {
